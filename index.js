d4c6b32dbbfd5b52536004bb78b9e45e832e849d
simretB05 <happysimret@gmail.com>
2021-06-14 13:07:27 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let x = document.createElement("h1");
+x.classList.add("title");
+x.innerHTML = `hello simret`;
+
+document.querySelector("body").append(x);
+console.log(x);
+function itsWindy(speed, unit) {
+  if (speed > 5 && unit === "metric") {
+    return true;
+  } else {
+    return false;
+  }
+}
+let speed = prompt("what is the wind speed");
+let unit = prompt("what is the unit metric or imperal?");
+if (itsWindy(speed, unit)) {
+  alert("it is windy");
+} else {
+  document.querySelector(".title").style.borderColor = "red";
+  document.querySelector(".title").style.color="yellow";
+}
