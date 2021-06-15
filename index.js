eb25acea14ae448382658ba5dd07150e412d6f29
simretB05 <happysimret@gmail.com>
2021-06-15 14:10:14 -0600

    added event listener

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ let c = document.createElement("p");
 c.classList.add("title-text");
 document.querySelector(".title").append(c);
 c.innerHTML = `you got this`;
+let btn = document.createElement("button");
+btn.setAttribute("id", "button");
+btn.innerHTML = `click me`;
+document.querySelector(".title").append(btn);
+let ul = document.createElement("ul");
+ul.classList.add("list");
+document.querySelector(".title").append(ul);
+let li = document.createElement("li");
+li.classList.add("list__item");
+li.innerHTML = `bravo`;
+document.querySelector(".list").appendChild(li);
+
 function itsWindy(speed, unit) {
   if (speed > 5 && unit === "metric") {
     return true;
@@ -27,7 +39,9 @@ if (itsWindy(speed, unit)) {
   document.querySelector(".title-text").style.color = "black";
   document.querySelector(".title-text").style.backgroundColor = "lightblue";
 }
-const button = document.querySelector("button");
-button.addEventListener("click", (e) => {
-  console.log(`event fired:{(e)}`);
+
+document.getElementById("button").addEventListener("click", (e) => {
+  let newli = document.createElement("li");
+  newli.innerHTML = `bravo simret!!`;
+  document.querySelector(".list").appendChild(newli);
 });
